e0d706caafb4d629fead0b80d41050fc0b1927b0
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-09 18:39:43 -0300

    update terraform files

--- modules/aws/eks_cluster/locals.tf
+++ modules/aws/eks_cluster/locals.tf
@@ -1,10 +1,10 @@
 locals {
   kubeconfig = templatefile("${path.module}/templates/kubeconfig.tpl", {
     kubeconfig_name                   = module.cluster.cluster_name
-    endpoint                          = coalescelist(module.cluster.endpoint, [""])[0]
-    cluster_auth_base64               = coalescelist(module.cluster.certificate_authority[0].data, [""])[0]
+    endpoint                          = module.cluster.endpoint
+    cluster_auth_base64               = module.cluster.certificate_authority[0].data
     aws_authenticator_command         = var.kubeconfig_aws_authenticator_command
-    aws_authenticator_command_args    = length(var.kubeconfig_aws_authenticator_command_args) > 0 ? var.kubeconfig_aws_authenticator_command_args : ["token", "-i", coalescelist(module.cluster.cluster_name, [""])[0]]
+    aws_authenticator_command_args    = length(var.kubeconfig_aws_authenticator_command_args) > 0 ? var.kubeconfig_aws_authenticator_command_args : ["token", "-i", module.cluster.cluster_name]
     aws_authenticator_additional_args = var.kubeconfig_aws_authenticator_additional_args
     aws_authenticator_env_variables   = var.kubeconfig_aws_authenticator_env_variables
   })
