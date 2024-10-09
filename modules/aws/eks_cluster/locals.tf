9f63a1629a0144938ea35c5b880b0e49dbd9930a
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-09 18:56:49 -0300

    teste

--- modules/aws/eks_cluster/locals.tf
+++ modules/aws/eks_cluster/locals.tf
@@ -1,8 +1,8 @@
 locals {
   kubeconfig = templatefile("${path.module}/templates/kubeconfig.tpl", {
-    kubeconfig_name = module.cluster.cluster_name
-    endpoint        = module.cluster.endpoint
-    # cluster_auth_base64               = base64encode(module.cluster.certificate_authority[0].data)
+    kubeconfig_name     = module.cluster.cluster_name
+    endpoint            = module.cluster.endpoint
+    cluster_auth_base64 = module.cluster.certificate_authority[0].data
     # aws_authenticator_command         = var.kubeconfig_aws_authenticator_command
     # aws_authenticator_command_args    = length(var.kubeconfig_aws_authenticator_command_args) > 0 ? var.kubeconfig_aws_authenticator_command_args : ["token", "-i", module.cluster.cluster_name]
     # aws_authenticator_additional_args = var.kubeconfig_aws_authenticator_additional_args
