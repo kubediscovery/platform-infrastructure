b0ffa85c5d9ecc7f548f880fa65242e378428988
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-09 19:17:07 -0300

    teste

--- modules/aws/eks_cluster/locals.tf
+++ modules/aws/eks_cluster/locals.tf
@@ -6,9 +6,5 @@ locals {
     kubeconfig_id       = module.cluster.cluster_name
     endpoint            = module.cluster.endpoint
     cluster_auth_base64 = module.cluster.certificate_authority[0].data
-    # aws_authenticator_command         = var.kubeconfig_aws_authenticator_command
-    # aws_authenticator_command_args    = length(var.kubeconfig_aws_authenticator_command_args) > 0 ? var.kubeconfig_aws_authenticator_command_args : ["token", "-i", module.cluster.cluster_name]
-    # aws_authenticator_additional_args = var.kubeconfig_aws_authenticator_additional_args
-    # aws_authenticator_env_variables   = var.kubeconfig_aws_authenticator_env_variables
   })
 }
