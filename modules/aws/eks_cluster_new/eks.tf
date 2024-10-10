2ddc2a02a2107f4b8a17a14fc7c0246d30f85e57
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-10 16:53:44 -0300

    update platform module branch

--- modules/aws/eks_cluster_new/eks.tf
+++ modules/aws/eks_cluster_new/eks.tf
@@ -8,61 +8,65 @@ module "eks" {
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  create_kms_key              = false
+  create_cloudwatch_log_group = false
+  cluster_encryption_config   = {}
 
   enable_irsa = true
 
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.private_subnets
+
+  # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     instance_types = ["t3.medium"]
     disk_size      = 50
   }
 
   eks_managed_node_groups = {
-    nodes = {
+    blue = {}
+    green = {
       min_size     = 1
       max_size     = 1
       desired_size = 1
 
       instance_types = ["t3.medium"]
+      capacity_type  = "SPOT"
     }
   }
 
-
-
-  # create_kms_key              = false
-  # create_cloudwatch_log_group = false
-  # cluster_encryption_config   = {}
-
-
-  # cluster_addons = {
-  #   coredns = {
-  #     most_recent = true
-  #   }
-  #   kube-proxy = {
-  #     most_recent = true
-  #   }
-  #   vpc-cni = {
-  #     most_recent = true
-  #   }
-  # }
-
-
-  # control_plane_subnet_ids = module.vpc.private_subnets
-
-  # # EKS Managed Node Group(s)
-
-
-  # node_security_group_additional_rules = {
-  #   ingress_self_all = {
-  #     description = "Node to node all ports/protocols"
-  #     protocol    = "-1"
-  #     from_port   = 0
-  #     to_port     = 0
-  #     type        = "ingress"
-  #     self        = true
-  #   }
-  # }
+  node_security_group_additional_rules = {
+    ingress_self_all = {
+      description = "Node to node all ports/protocols"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "ingress"
+      self        = true
+    }
+    # egress_all = {
+    #   description      = "Node all egress"
+    #   protocol         = "-1"
+    #   from_port        = 0
+    #   to_port          = 0
+    #   type             = "egress"
+    #   cidr_blocks      = ["0.0.0.0/0"]
+    #   ipv6_cidr_blocks = ["::/0"]
+    # }
+  }
 
   tags = var.tags
 }
