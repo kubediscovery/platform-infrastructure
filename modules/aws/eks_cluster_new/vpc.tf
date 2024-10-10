7ea73f3ee0d4184d96b11fec10d9aaf72c11b7ee
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-10 11:38:53 -0300

    update platform module branch

--- modules/aws/eks_cluster_new/vpc.tf
+++ modules/aws/eks_cluster_new/vpc.tf
@@ -2,15 +2,15 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.0.0"
 
-  name = "eks-vpc"
-  cidr = "10.0.0.0/16"
+  name = "${var.project_name}-vpc"
+  cidr = var.cidr_block
 
 
   azs = ["us-east-1a", "us-east-1b"]
   #private_subnets     = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
   #public_subnets      = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 4)]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  private_subnets = [cidrsubnet(var.cidr_block, 7, 1), cidrsubnet(var.cidr_block, 7, 2)]
+  public_subnets  = [cidrsubnet(var.cidr_block, 7, 3), cidrsubnet(var.cidr_block, 7, 4)]
 
   enable_nat_gateway = true
 
@@ -22,8 +22,5 @@ module "vpc" {
     "kubernetes.io/role/internal-elb" = 1
   }
 
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
+  tags = var.tags
 }
