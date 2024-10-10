23503bbd15752a39b81f0e83c192536e82d78374
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-10 11:08:13 -0300

    update platform module branch

--- modules/platform/labs/module.tf
+++ modules/platform/labs/module.tf
@@ -1,6 +1,6 @@
 
 module "platform" {
-  source = "git::https://github.com/kubediscovery/platform-infrastructure.git//modules/aws/eks_cluster?ref=develop"
+  source = "git::https://github.com/kubediscovery/platform-infrastructure.git//modules/aws/eks_cluster_new?ref=develop"
 
   project_name = var.project_name
   cidr_block   = var.cidr_block
