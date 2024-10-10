bb84573abfb73d7ec22c70d36519a149bd155e14
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-10 12:58:52 -0300

    update platform module branch

--- modules/aws/eks_cluster_new/providers.tf
+++ modules/aws/eks_cluster_new/providers.tf
@@ -6,9 +6,28 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.61"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.32.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.14.0"
+    }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
+    command     = "aws"
+  }
 }
