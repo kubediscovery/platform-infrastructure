e632892f1ce7cc4c678116b60fbad65c5094d8cb
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-10 17:41:48 -0300

    update platform module branch

--- modules/aws/eks_cluster_new/eks.tf
+++ modules/aws/eks_cluster_new/eks.tf
@@ -2,7 +2,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "${var.project_name}-al2"
+  cluster_name    = "${var.project_name}-eks"
   cluster_version = "1.30"
 
   # EKS Addons
@@ -22,8 +22,20 @@ module "eks" {
       ami_type       = "AL2_x86_64"
       instance_types = ["t3.medium"]
 
-      min_size = 2
-      max_size = 5
+      min_size = 1
+      max_size = 3
+      # This value is ignored after the initial creation
+      # https://github.com/bryantbiggs/eks-desired-size-hack
+      desired_size = 2
+    }
+    platform = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type = "AL2_x86_64"
+
+      instance_types = ["t3.medium"]
+
+      min_size = 1
+      max_size = 2
       # This value is ignored after the initial creation
       # https://github.com/bryantbiggs/eks-desired-size-hack
       desired_size = 2
