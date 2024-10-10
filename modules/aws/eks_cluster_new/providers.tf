b01fde5aa997c5468f6fa9088742cadaa34339d1
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-10 13:43:52 -0300

    update platform module branch

--- modules/aws/eks_cluster_new/providers.tf
+++ modules/aws/eks_cluster_new/providers.tf
@@ -26,23 +26,24 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.default.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data[0].data)
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
     command     = "aws"
   }
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.default.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data[0].data)
+
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
       command     = "aws"
     }
   }
