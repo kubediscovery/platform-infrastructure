872b9e71cf88f0eeed113eb5f59e502411dda35a
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-10 16:37:00 -0300

    update platform module branch

--- modules/aws/eks_cluster_new/eks.tf
+++ modules/aws/eks_cluster_new/eks.tf
@@ -48,16 +48,25 @@ module "eks" {
     }
   }
 
-  # node_security_group_additional_rules = {
-  #   ingress_allow_access_from_control_plane = {
-  #     type                          = "ingress"
-  #     protocol                      = "tcp"
-  #     from_port                     = 9443
-  #     to_port                       = 9443
-  #     source_cluster_security_group = true
-  #     description                   = "Allow access from control plane to webhook port of AWS load balancer controller"
-  #   }
-  # }
+  node_security_group_additional_rules = {
+    ingress_self_all = {
+      description = "Node to node all ports/protocols"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "ingress"
+      self        = true
+    }
+    egress_all = {
+      description      = "Node all egress"
+      protocol         = "-1"
+      from_port        = 0
+      to_port          = 0
+      type             = "egress"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  }
 
   tags = var.tags
 }
