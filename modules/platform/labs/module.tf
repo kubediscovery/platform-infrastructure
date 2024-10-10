9bbdaecc3e0dfe3ba50b7ab0f134a6e66061b0fb
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-10 10:45:04 -0300

    update platform module branch

--- modules/platform/labs/module.tf
+++ modules/platform/labs/module.tf
@@ -1,5 +1,20 @@
 locals {
-  provider = var.cloud_provider == "aws" ? 1 : 0
+  aws_enabled   = var.cloud_provider == "aws"
+  azure_enabled = var.cloud_provider == "azure"
+}
+
+output "enabled_provider" {
+  value = {
+    "azure" = local.azure_enabled
+    "aws"   = local.aws_enabled
+  }
+}
+resource "null_resource" "aws" {
+  count = local.aws_enabled ? 1 : 0
+}
+
+resource "null_resource" "azure" {
+  count = local.azure_enabled ? 1 : 0
 }
 
 module "platform" {
