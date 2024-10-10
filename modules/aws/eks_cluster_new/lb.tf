2b986b1de7e0947cc11349cd72c0b97aaccf0f0c
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-10 13:39:05 -0300

    update platform module branch

--- modules/aws/eks_cluster_new/lb.tf
+++ modules/aws/eks_cluster_new/lb.tf
@@ -27,18 +27,18 @@ resource "helm_release" "aws_load_balancer_controller" {
     value = 1
   }
 
-  set {
-    name  = "clusterName"
-    value = module.eks.cluster_id
-  }
+  # set {
+  #   name  = "clusterName"
+  #   value = module.eks.cluster_id
+  # }
 
-  set {
-    name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
-  }
+  # set {
+  #   name  = "serviceAccount.name"
+  #   value = "aws-load-balancer-controller"
+  # }
 
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = module.aws_load_balancer_controller_irsa_role.iam_role_arn
-  }
+  # set {
+  #   name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+  #   value = module.aws_load_balancer_controller_irsa_role.iam_role_arn
+  # }
 }
